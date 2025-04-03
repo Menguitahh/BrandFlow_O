f0ad4084217ca066f5673942508c44d4481f773e
Luciano Mengarelli <144611932+Menguitahh@users.noreply.github.com>
2025-04-03 17:33:49 -0300

    Modificaciones de algunas tablas

--- BrandFlow_/admin.py
+++ BrandFlow_/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Brand)
-admin.site.register(project) 
-admin.site.register(designvisual)   
+admin.site.register(User)
+admin.site.register(CampaignUser) 
+admin.site.register(Campaingn)
+admin.site.register(Category)
+admin.site.register(Order)
+# admin.site.register()       
 #ajustar
 # Register your models here.
