ec26a8db35a49ab1d01c85a133cbe74bee15a49c
Luciano Mengarelli <144611932+Menguitahh@users.noreply.github.com>
2025-04-03 16:34:34 -0300

    Conexion a la base de datos +

--- BrandFlow_/admin.py
+++ BrandFlow_/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
+admin.site.register(Brand)
+admin.site.register(project) 
+admin.site.register(designvisual)   
+#ajustar
 # Register your models here.
--- BrandFlow_/models.py
+++ BrandFlow_/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class project(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=30)
+
+    def __str__(self):
+        return self.name
+    
+class Brand(models.Model):
+    name = models.CharField(max_length=15)
+    description = models.TextField()
+    logo = models.ImageField(upload_to='logos/')
+    website = models.URLField(max_length=200)
+    email = models.EmailField(max_length=30)
+    phone = models.CharField(max_length=15)
+    def __str__(self):
+        return self.name
+    
+class designvisual(models.Model):
+    name = models.CharField(max_length=15)
+    email = models.EmailField(max_length=30)
+    
+    def __str__(self):
+        return self.name
